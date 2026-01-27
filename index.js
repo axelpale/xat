bcfd25469830e29a823dcee945276985554cd5ac
Akseli Palen <akseli.palen@gmail.com>
2026-01-27 22:18:10 +0200

    sketch processRows further

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const readRows = require('./lib/readRows')
-const AccountCollection = require('./AccountCollection')
+const AccountCollection = require('./lib/AccountCollection')
 const processRows = require('./lib/processRows')
 
 const main = async function () {
   const rows = await readRows('transaction-history.csv')
 
   const accounts = new AccountCollection()
-  processRows(accounts, rows)
+  const result = processRows(accounts, rows)
 
-  console.log(accounts)
+  console.log(result)
 }
 
 main()

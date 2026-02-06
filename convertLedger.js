a4904cdc6c8c10308c20520b66036f15f22f40cf
Akseli Palen <akseli.palen@gmail.com>
2026-02-07 01:24:22 +0200

    minimal viable ledger reader and formatter

--- convertLedger.js
+++ convertLedger.js
@@ -1,6 +1,25 @@
+const fs = require('fs')
 const BigNumber = require('big.js')
+const chalk = require('chalk').default
 const ZERO = new BigNumber(0)
 const readLedger = require('./lib/readLedger')
+
+const pickCommon = (prop) => {
+  // Returns a reducer function that returns the property value
+  // that is the same for all objects in the reduced array.
+  // Errors if the value is not the same.
+  //
+  return (acc, x) => {
+    if (!acc) {
+      return x[prop]
+    }
+    if (acc === x[prop]) {
+      return acc
+    }
+    throw new Error('Unexpected multi-valued ' + prop + ' property ' +
+      'among array elements.')
+  }
+}
 
 const main = async function () {
   const ledgerRows = await readLedger('ledger.csv')
@@ -41,7 +60,7 @@ const main = async function () {
       return {
         date: ref.time,
         voucher: [],
-        type: '' + ref.type,
+        type: `${ref.type}_${ref.subtype}`,
         desc: `${ref.type} ${ref.subtype} ${ref.asset} ${ref.wallet}`,
         protocol: 'Exchange',
         txid: ref.txid,
@@ -51,17 +70,17 @@ const main = async function () {
         toAccount: accountName,
         sentAmount: null,
         sentUnit: '',
-        sentUnitPriceEur: null,
         receivedAmount: ref.amount,
         receivedUnit: ref.asset,
-        receivedUnitPriceEur: null,
-        feeAccount: accountName,
         feeAmount: ref.fee,
         feeUnit: ref.asset,
-        feeUnitValueEur: null,
+        feeAccount: accountName,
         exchangeRate: null,
         exchangeRateUnit: null,
         rateUsdEur: null,
+        sentUnitPriceEur: null,
+        receivedUnitPriceEur: null,
+        feeUnitPriceEur: null,
         senderBalance: null,
         receiverBalance: ref.balance
       }
@@ -83,43 +102,118 @@ const main = async function () {
       throw new Error('Unexpectedly no receiver in a multi-reference set.')
     }
 
-    const sentAmount = senders.reduce((acc, x) => acc.plus(x.amount), ZERO)
-    const recAmount = receivers.reduce((acc, x) => acc.plus(x.amount), ZERO)
+    const sentUnit = senders.reduce(pickCommon('asset'), null)
+    const recUnit = receivers.reduce(pickCommon('asset'), null)
 
-    const pickCommon = (acc, x) => {
-      if (!acc) {
-        return x.asset
-      }
-      if (acc === x.asset) {
-        return acc
-      }
-      throw new Error('Unexpected multi-unit assets in a reference set.')
+    const sentAmount = senders.reduce((acc, x) => {
+      return acc.plus(x.amount)
+    }, ZERO).abs()
+    const recAmount = receivers.reduce((acc, x) => {
+      return acc.plus(x.amount)
+    }, ZERO)
+
+    const sentFee = senders.reduce((acc, x) => {
+      return acc.plus(x.fee.abs())
+    }, ZERO)
+    const recFee = receivers.reduce((acc, x) => {
+      return acc.plus(x.fee.abs())
+    }, ZERO)
+
+    if (sentFee.gt(0) && recFee.gt(0)) {
+      console.log(chalk.orange('warning:') + ' multi-unit fee')
     }
-    const sentUnit = senders.reduce(pickCommon, null)
-    const recUnit = receivers.reduce(pickCommon, null)
+    let feeUnit = null
+    let feeAmount = null
+    if (sentFee.gt(0)) {
+      feeUnit = sentUnit
+      feeAmount = sentFee
+    } else if (recFee.gt(0)) {
+      feeUnit = recUnit
+      feeAmount = recFee
+    } else {
+      feeUnit = sentUnit
+      feeAmount = ZERO
+    }
+
+    // Follow the exchange habit: sent amount already after fee.
+    const sentAmountLessFee = sentAmount
+    // Received amount before fee by default.
+    let recAmountLessFee = recAmount
+    if (recFee.gt(0)) {
+      recAmountLessFee = recAmount.minus(recFee)
+    }
+
+    const senderWallet = senders.reduce(pickCommon('wallet'), null)
+    const receiverWallet = receivers.reduce(pickCommon('wallet'), null)
+    const senderAccount = `Exchange ${senderWallet} ${sentUnit}`
+    const receiverAccount = `Exchange ${receiverWallet} ${recUnit}`
+
+    const refType = refSet.reduce(pickCommon('type'), null)
     const lastRef = (arr) => arr[arr.length - 1]
+
     const senderBalance = lastRef(senders).balance
     const receiverBalance = lastRef(receivers).balance
+
+    const description = `${refType} from ${sentUnit} to ${recUnit}`
+
     return {
       date: firstRef.time,
       voucher: [],
-      type: firstRef.type,
+      type: `${firstRef.type}_${firstRef.subtype}`,
+      desc: description,
       protocol: 'Exchange',
       txid: firstRef.txid,
       fromAddress: '',
       toAddress: '',
-      sentAmount,
+      fromAccount: senderAccount,
+      toAccount: receiverAccount,
+      sentAmount: sentAmountLessFee,
       sentUnit,
-      receivedAmount: recAmount,
+      receivedAmount: recAmountLessFee,
       receivedUnit: recUnit,
-      feeAmount: null,
-      feeUnit: null,
+      feeAmount,
+      feeUnit,
+      feeAccount: null,
+      exchangeRate: null,
+      exchangeRateUnit: null,
+      rateUsdEur: null,
+      sentUnitPriceEur: null,
+      receivedUnitPriceEur: null,
+      feeUnitPriceEur: null,
       senderBalance,
       receiverBalance
     }
   })
 
-  console.log(rows)
+  // Write a CSV file.
+  const firstRow = rows[0]
+  const columnNames = Object.keys(firstRow)
+  const labelLine = columnNames.join(',')
+  const pretty = (x) => {
+    if (!x) {
+      return ''
+    }
+    if (typeof x === 'string') {
+      return '"' + x + '"'
+    }
+    if (typeof x === 'object') {
+      if (x.toFixed) {
+        return x.toFixed(8)
+      }
+      if (x.toString) {
+        return '"' + x.toString() + '"'
+      }
+    }
+    if (typeof x === 'number') {
+      return x.toFixed(8)
+    }
+    return ''
+  }
+  const reversedRows = rows.toReversed()
+  const fileString = reversedRows.reduce((acc, r) => {
+    return acc + columnNames.map(k => pretty(r[k])).join(',') + '\n'
+  }, labelLine + '\n')
+  fs.writeFileSync('ledger-history.csv', fileString)
 }
 
 main()

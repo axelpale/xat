bfe991d6a0d5708e8f53a9b0a23ea993f73fad19
Akseli Palen <akseli.palen@gmail.com>
2026-02-07 16:20:11 +0200

    implement column filter

--- convertLedger.js
+++ convertLedger.js
@@ -1,6 +1,7 @@
 const readLedger = require('./lib/ledger/readLedger')
 const convertToRows = require('./lib/ledger/convertToRows')
 const joinSimilarRows = require('./lib/ledger/joinSimilarRows')
+const filterColumns = require('./lib/ledger/filterColumns')
 const writeTransactionHistory = require('./lib/writeTransactionHistory')
 
 const main = async function () {
@@ -11,8 +12,27 @@ const main = async function () {
 
   // Join similar rows so that the resulting spreadsheet is more manageable.
   const denseRows = joinSimilarRows(rows)
+
+  // Strip unnecessary columns for easier debugging.
+  const columns = [
+    'date',
+    'type',
+    'desc',
+    'fromAccount',
+    'toAccount',
+    'sentAmount',
+    'sentUnit',
+    'receivedAmount',
+    'receivedUnit',
+    'feeAmount',
+    'feeUnit',
+    'senderBalance',
+    'receiverBalance'
+  ]
+  const denserRows = filterColumns(denseRows, columns)
+
   // Write a CSV file.
-  writeTransactionHistory(rows, 'ledger-history.csv')
+  writeTransactionHistory(denserRows, 'ledger-history.csv')
 }
 
 main()

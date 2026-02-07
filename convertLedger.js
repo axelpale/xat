9466f44b56178330522414ae6be217929941b4f4
Akseli Palen <akseli.palen@gmail.com>
2026-02-07 16:19:51 +0200

    implement row joining

--- convertLedger.js
+++ convertLedger.js
@@ -1,5 +1,6 @@
 const readLedger = require('./lib/ledger/readLedger')
 const convertToRows = require('./lib/ledger/convertToRows')
+const joinSimilarRows = require('./lib/ledger/joinSimilarRows')
 const writeTransactionHistory = require('./lib/writeTransactionHistory')
 
 const main = async function () {
@@ -8,6 +9,8 @@ const main = async function () {
   // Process the ledger rows. Chronological order. Connect by reference id.
   const rows = convertToRows(ledgerRows)
 
+  // Join similar rows so that the resulting spreadsheet is more manageable.
+  const denseRows = joinSimilarRows(rows)
   // Write a CSV file.
   writeTransactionHistory(rows, 'ledger-history.csv')
 }

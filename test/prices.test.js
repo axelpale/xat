608c16508824ca43241292bd7fd541b6a6aa12ff
Akseli Palen <akseli.palen@gmail.com>
2026-02-13 03:21:16 +0200

    refactor price history to allow sync query

--- test/prices.test.js
+++ test/prices.test.js
@@ -3,17 +3,21 @@ const prices = require('../lib/prices')
 
 test('prices: ensure module structure', (t) => {
   t.ok(typeof prices.getPriceEur === 'function', 'should have methods')
+  t.ok(typeof prices.loadPriceHistory === 'function', 'should have methods')
 
   t.end()
 })
 
 test('prices:getPriceEur behavior', async (t) => {
+  // Read the prices
+  await prices.loadPriceHistory('USDC')
+
   // Should be 0.918735090591478
-  const p = await prices.getPriceEur('USDC', '2020-04-20')
+  const p = prices.getPriceEur('USDC', '2020-04-20')
   t.ok(p.lt('0.919'), 'should be in range')
   t.ok(p.gt('0.912'), 'should be in range')
 
-  const q = await prices.getPriceEur('hello', '2020-04-20')
+  const q = prices.getPriceEur('hello', '2020-04-20')
   t.ok(q === null, 'missing unit should not throw')
 
   t.end()

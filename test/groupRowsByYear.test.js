3b6e53f5d7e9ca30e23e930c62e71e9d8593c709
Akseli Palen <akseli.palen@gmail.com>
2026-03-18 14:30:21 +0200

    implement util groupRowsByYear

---
+++ test/groupRowsByYear.test.js
@@ -0,0 +1,19 @@
+const test = require('tape')
+const groupRowsByYear = require('../lib/utils/groupRowsByYear')
+
+test('groupRowsByYear: ', (t) => {
+  const mockRows = [
+    { date: '2014-01-01' },
+    { date: '2014-03-13' },
+    { date: '2015-05-15' },
+    { date: '2020-02-20' }
+  ]
+
+  const groups = groupRowsByYear(mockRows)
+
+  t.equal(groups.length, 3, 'should divide to groups')
+  t.equal(groups[0].length, 2, 'should maintain order')
+  t.equal(groups[2].length, 1, 'should maintain order')
+
+  t.end()
+})

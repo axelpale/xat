2e56b892fc6ccff50dc25353cd3f64b183bd7c13
Akseli Palen <akseli.palen@gmail.com>
2026-03-19 12:26:51 +0200

    refactor: sort module indices alphabetically

--- test/index.js
+++ test/index.js
@@ -1,9 +1,12 @@
-require('./Asset.test.js')
-require('./GiftAsset.test.js')
-require('./groupRowsByYear.test.js')
-require('./getYearFromDateTime.test.js')
+// Test utilities
 require('./diffDays.test.js')
+require('./getYearFromDateTime.test.js')
+require('./groupRowsByYear.test.js')
 require('./inRange.test.js')
 require('./inSpread.test.js')
+// Classes
+require('./Asset.test.js')
+require('./GiftAsset.test.js')
+// High-level process
 require('./prices.test.js')
 require('./processRows.test.js')
